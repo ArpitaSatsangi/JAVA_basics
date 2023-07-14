05616c2fa9c2e3a507f8a8bbd99df7a23298798d
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-07-14 23:08:01 +0530

    Create

---
+++ 01. Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+
+        int num1=2;
+        int num2=5;
+        int num3=1;
+        int sum=num1+num2+num3;
+
+        System.out.print("The sum of three numbers is: ");
+        System.out.print(sum);
+
+
+    }
+}
