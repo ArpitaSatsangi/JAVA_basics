674c35589716f4d3da63761d76c1a37dc7c04e56
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-07-15 23:28:56 +0530

    Create

---
+++ 02. Literals.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Literals {
+
+    public static void main(String[] args)
+    {
+        byte age = 34;
+        int age2 = 36;
+        short age3 = 89;
+        long ageDino = 32222222222222223L;
+        char ch = 'A';
+        float f1 = 2.32f;
+        double d = 3.22;
+
+        boolean a = true;
+
+        System.out.println(age);
+        String str = "YELO";
+        System.out.println(str);
+    }
+}
