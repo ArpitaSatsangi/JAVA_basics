8a9d65ed66b4027aa77395f9ea624c2f1c4c63e1
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-07-16 23:20:13 +0530

    Add files via upload

---
+++ 03. takingInput.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+
+class takingInput {
+    
+    
+public static void main(String[] args) {
+        
+System.out.println("HEYY!");
+        
+        
+Scanner sc = new Scanner(System.in);
+        
+System.out.print("Enter number 1:  ");
+        
+float a = sc.nextFloat();
+    
+ System.out.print("Enter number 2:  ");
+        
+float b = sc.nextFloat();
+   
+
+float c= a+b;
+System.out.println("The sum of numbers is: ");
+System.out.print(c);       
+ 
+  
+//String str1 = sc.next(); //word
+
+System.out.println("Enter a sentence:  ");
+   
+String str = sc.nextLine();
+        System.out.println(str); 
+     
+
+
+ }
+
+
+}
