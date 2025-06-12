4da91a1a168b63d57a5824625356ca08c64854bf
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2025-06-12 23:23:17 +0530

    Create 08. switch.java

---
+++ 08. switch.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+        
+        String day="Fridayy";
+        
+        switch(day) {
+            case "Sunday" : System.out.println("Sunday");
+            break;
+            
+            case "Friday" : System.out.println("Friday");
+            break;
+            
+            default : System.out.println("Not a day");
+        }
+    }
+}
