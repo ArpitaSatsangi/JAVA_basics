3e0477c69d165046ac3802fc4d0248a93daf4b5a
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2025-06-13 12:09:42 +0530

    Create 13.wrapper class.java

---
+++ 13.wrapper class.java
@@ -0,0 +1,22 @@
+class Main {
+    public static void main(String[] args) {
+    
+/* Wrapper classes in Java are classes that encapsulate primitive data types, allowing them to be treated as objects. 
+Java's primitive data types (like int, char, float, etc.) are not objects, 
+but wrapper classes provide a way to convert them into objects when needed. 
+Autoboxing - automatic conversion that java makes bw premitive to wrapper class object
+Unboxing - reverse of autoboxing
+*/
+
+    
+        Boolean a = true;
+        Character b = '@';
+        Integer c =123;
+        Double d = 3.14;
+        String e = "Bro";
+        
+        if(a==true) {
+            System.out.println("This is true");
+        }
+    } 
+}
