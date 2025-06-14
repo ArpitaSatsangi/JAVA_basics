552e7993f97491c86bd0ee54856c48781d4be725
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2025-06-14 15:01:39 +0530

    Create 17. final.java

---
+++ 17. final.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        
+        final double PI = 3.14159;
+        // PI = 4; error: cannot assign a value to final variable PI
+        System.out.println(PI);
+    }
+}
